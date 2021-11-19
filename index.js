c45adf6120a7d7cd519cbe1d95f4bcb46812c856
debashismazumderbd <debashismazumder.bd@gmail.com>
2021-11-19 22:31:22 +0600

    code edit

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.get('/', (req, res) => {
 })
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g5qv1.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+console.log(uri);
 client.connect(err => {
     const serviceCollection = client.db(`${process.env.DB_NAME}`).collection("services");
     const reviewCollection = client.db(`${process.env.DB_NAME}`).collection("reviews");
